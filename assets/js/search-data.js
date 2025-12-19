9a2fdec809a2a2bf0786bab2f249c8ebbe13ba3b
chenxxxing <31952927+chenxxxing@users.noreply.github.com>
2025-12-19 22:12:10 +0000

    Deploying to gh-pages from @ chenxxxing/chenxxxing.github.io@bcb6f481fa3faeef7293434ed9f1a587a4108b29 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -376,16 +376,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2.html";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-a-hard-look-at-geoengineering-reveals-global-risks-the-current",
+          title: '“A hard look at geoengineering reveals global risks” The Current',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
