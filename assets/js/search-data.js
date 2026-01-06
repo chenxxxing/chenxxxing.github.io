ae621ea2716df33642a5c22c325919c938072bce
chenxxxing <31952927+chenxxxing@users.noreply.github.com>
2026-01-06 17:35:12 +0000

    Deploying to gh-pages from @ chenxxxing/chenxxxing.github.io@f0d20ecc177d2ff9bcd8092fd1fefc28839d45aa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-projects-underconstruction",
+          title: "projects (UNDERCONSTRUCTION)",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
